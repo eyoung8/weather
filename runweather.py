af79fc0a53c8ed51d4c3ec7111e01cc408e00a4e
eyoung8 <eyoung8@binghamton.edu>
2015-02-10 21:36:53 -0500

    adding working weather script + runweather.py which runs weather with my choice of options

---
+++ runweather.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+
+#import weather
+import sys
+import os
+import subprocess
+print ('\n')
+os.system("python3 weather.py binghamton ny location temp windchill condition high low humidity date")
+print('\n')
